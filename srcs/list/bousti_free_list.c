8eec55c20a22f913c8281fcfe2b38157c3d3079b
ungaro_l <ungaro_l@epitech.eu>
2016-04-28 13:38:35 +0200

    free_list and circ_list updates

--- srcs/list/bousti_free_list.c
+++ srcs/list/bousti_free_list.c
@@ -5,7 +5,7 @@
 ** Login   <ungaro_l@epitech.net>
 ** 
 ** Started on  Fri Oct 30 18:39:10 2015 Luca Ungaro
-** Last update Thu Apr 14 11:21:00 2016 Luca Ungaro
+** Last update Sun Apr 24 21:57:39 2016 Luca Ungaro
 */
 #include "bouf.h"
 
@@ -29,7 +29,7 @@ void		bousti_free_list_fct(t_list *list,
 {
   t_list	*tmp;
 
-  while (list)
+  while (list && list->data)
     {
       tmp = list;
       list = list->next;
--- srcs/list/bousti_put_in_circ.c
+++ srcs/list/bousti_put_in_circ.c
@@ -5,7 +5,7 @@
 ** Login   <ungaro_l@epitech.net>
 ** 
 ** Started on  Wed Dec  2 10:21:18 2015 Luca Ungaro
-** Last update Mon Apr 18 17:05:05 2016 Luca Ungaro
+** Last update Sun Apr 24 23:23:13 2016 Luca Ungaro
 */
 
 #include "bouf.h"
@@ -27,28 +27,16 @@ t_list		*bousti_create_root(void)
 int		bousti_put_in_end_dbcirc(t_list **list, void *data)
 {
   int		ret;
-  t_list	*new_elem;
-  t_list	*root;
+  t_list	*tmp;
+  t_list	*prev;
 
-  ret = 1;
-  new_elem = std_malloc(sizeof(t_list));
-  if (new_elem)
+  ret = bousti_put_in_end_circ(list, data);
+  tmp = bousti_get_root(*list);
+  prev = NULL;
+  while (tmp && (tmp = tmp->next) && tmp->data)
     {
-      new_elem->data = data;
-      root = bousti_get_root(*list);
-      new_elem->next = root;
-      if (root->prev)
-	{
-	  new_elem->prev = root->prev;
-	  new_elem->prev->next = new_elem;
-	}
-      else
-	new_elem->prev = root;
-      if (*list == root)
-	*list = new_elem;
-      root->prev = new_elem;
-      root->next = *list;
-      ret = 0;
+      tmp->prev = prev;
+      prev = tmp;
     }
   return (ret);
 }
@@ -66,6 +54,7 @@ int		bousti_put_in_dbcirc(t_list **list, void *data)
       new_elem->data = data;
       root = bousti_get_root(*list);
       new_elem->next = *list;
+      new_elem->prev = root;
       (*list)->prev = new_elem;
       *list = new_elem;
       root->next = *list;
@@ -77,19 +66,27 @@ int		bousti_put_in_dbcirc(t_list **list, void *data)
 int		bousti_put_in_end_circ(t_list **list, void *data)
 {
   int		ret;
+  t_list	*tmp;
   t_list	*new_elem;
   t_list	*root;
 
   ret = 1;
   new_elem = std_malloc(sizeof(t_list));
+  tmp = *list;
+  while (tmp && tmp->next && tmp->next->data)
+    tmp = tmp->next;
+  root = bousti_get_root(*list);
   if (new_elem)
     {
       new_elem->data = data;
-      root = bousti_get_root(*list);
       new_elem->next = root;
       if (*list == root)
-	*list = new_elem;
-      root->next = *list;
+	{
+	  *list = new_elem;
+	  root->next = *list;
+	}
+      else if (tmp)
+	tmp->next = new_elem;
       ret = 0;
     }
   return (ret);
@@ -109,6 +106,7 @@ int		bousti_put_in_circ(t_list **list, void *data)
       root = bousti_get_root(*list);
       new_elem->next = *list;
       root->next = new_elem;
+      *list = new_elem;
       ret = 0;
     }
   return (ret);
